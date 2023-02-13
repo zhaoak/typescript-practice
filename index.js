c1f758f904eb91a26d975aea2a92473a5b35881b
Allie Zhao <allie.k.zhao@gmail.com>
2023-02-13 14:52:04 -0800

    writing out some practice code for calculating line length segments from coordinates, nothing fancy

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+"use strict";
 function hiMom(momName) {
     return "Hi " + momName;
 }
-var user = { firstName: "mom", lasName: "momerson" };
-document.body.textContent = hiMom(user);
+let user = { firstName: "mom", lastName: "momerson" };
+function printPointCoordinates(point) {
+    return `(${point.x}, ${point.y})`;
+}
+function findLineLength(point1, point2) {
+    let length = (point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2;
+    length = Math.sqrt(length);
+    return length;
+}
+let zerozero = { x: 0, y: 0 };
+let oneone = { x: 1, y: 1 };
+console.log(printPointCoordinates(zerozero));
+console.log(findLineLength(zerozero, oneone));
--- index.ts
+++ index.ts
@@ -9,4 +9,22 @@ function hiMom(momName: Person) {
 
 let user = { firstName: "mom", lastName: "momerson" };
 
-document.body.textContent = hiMom(user);
+interface Point {
+  x: number;
+  y: number;
+}
+
+function printPointCoordinates(point: Point): string {
+  return `(${point.x}, ${point.y})`;
+}
+
+function findLineLength(point1: Point, point2: Point): number {
+  let length: number = (point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2;
+  length = Math.sqrt(length);
+  return length;
+}
+
+let zerozero: Point = { x: 0, y: 0 };
+let oneone: Point = { x: 1, y: 1 };
+console.log(printPointCoordinates(zerozero));
+console.log(findLineLength(zerozero, oneone));
